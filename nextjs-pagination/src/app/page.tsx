514303dab0501b6ecbaf1c84bb036b0494c7e0fd
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-06-25 18:28:53 -0300

    (nextjs-pagination) feat: add getPosts function

--- nextjs-pagination/src/app/page.tsx
+++ nextjs-pagination/src/app/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <h1>Hello World!</h1>;
+interface PostsProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function getPosts() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const response = data.json();
+
+  return response;
+}
+
+export default async function Home() {
+  const posts: PostsProps[] = await getPosts();
+
+  return (
+    <main className="max-w-4xl mx-auto my-4">
+      <div>
+        {posts && (
+          <ul className="space-y-2">
+            {posts.map((post) => (
+              <li key={post.id} className="border shadow-sm p-4">
+                <h1 className="text-xl font-bold">{post.title}</h1>
+                <p>{post.body}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </main>
+  );
 }
