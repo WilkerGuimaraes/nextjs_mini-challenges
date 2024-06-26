f5bb539a9364e7737d6a0dfc3f3ff1dcbe5d2640
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-06-26 18:24:44 -0300

    (nextjs-revalidation) feat: add json-server and getData function

--- nextjs-revalidation/src/app/page.tsx
+++ nextjs-revalidation/src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <h1>Hello World!</h1>;
+interface UserProps {
+  id: string;
+  name: string;
+  email: string;
+}
+
+async function getData() {
+  const response = await fetch("http://localhost:3333/users", {
+    next: { tags: ["get-users"] },
+  });
+  const data = response.json();
+
+  return data;
+}
+
+export default async function Home() {
+  const users: UserProps[] = await getData();
+
+  return (
+    <main className="mx-auto flex flex-col items-center gap-4 p-8 md:items-start">
+      <div className="w-full rounded-lg border border-zinc-300 p-6">
+        {users && (
+          <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            {users.map((user) => (
+              <li key={user.id} className="p-4 shadow-md">
+                <h1 className="text-xl font-bold">{user.name}</h1>
+                <p className="text-zinc-400">{user.email}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </main>
+  );
 }
